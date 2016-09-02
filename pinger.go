7303fd6bc8e27d90faa680f8911137001b5f8774
Nikolay Tashev <nikolay.tashev@techhuddle.net>
2016-09-02 17:15:52 +0300

    WPTA-622 Added time option to pinger

--- pinger.go
+++ pinger.go
@@ -14,7 +14,8 @@ import (
 func main() {
 	log.Print("Starting up...")
 
-	interval, settings, err := GetSettings()
+	interval, settings, timeToRun, err := GetSettings()
+
 	if err != nil {
 		log.Print(err)
 		os.Exit(1)
@@ -22,29 +23,60 @@ func main() {
 
 	client := &http.Client{}
 
-	for {
-		err := DoCall(client, settings)
-		if err != nil {
-			log.Print(err)
+	if time.Time.IsZero(timeToRun) {
+		for {
+			err := DoCall(client, settings)
+			if err != nil {
+				log.Print(err)
+			}
+			log.Printf("Sleeping for %d seconds", interval)
+			time.Sleep(time.Duration(interval) * time.Second)
 		}
-		log.Printf("Sleeping for %d seconds", interval)
-		time.Sleep(time.Duration(interval) * time.Second)
+	} else {
+		for {
+			duration := GetDurationToWait(interval, timeToRun)
+			log.Printf("Sleeping for %f seconds", duration.Seconds())
+			time.Sleep(duration)
+			err := DoCall(client, settings)
+			if err != nil {
+				log.Print(err)
+			}
+		}
 	}
 }
 
+//GetDurationToWait Get duration till next run
+func GetDurationToWait(interval int, timeToRun time.Time) time.Duration {
+	now := time.Now().UTC()
+	nextRun := time.Date(now.Year(), now.Month(), now.Day(), timeToRun.Hour(), timeToRun.Minute(), timeToRun.Second(), 0, time.UTC)
+
+	if !nextRun.After(now) {
+		nextRun = nextRun.Add(time.Duration(interval) * time.Second)
+	}
+
+	return nextRun.Sub(now)
+}
+
 // GetSettings Read required settings from environment variables
-func GetSettings() (int, map[string]string, error) {
+func GetSettings() (int, map[string]string, time.Time,  error) {
 	target := os.Getenv("TARGET_URL")
 	method := os.Getenv("METHOD")
-	interval, err := strconv.Atoi(os.Getenv("INTERVAL"))
+	interval, err1 := strconv.Atoi(os.Getenv("INTERVAL"))
 
-	if target == "" || method == "" || err != nil || interval < 0 {
-		return -1, map[string]string{}, errors.New("Environment variables were not set, returning error")
+	timeToRun := time.Time{}
+	var err2 error
+	if os.Getenv("TIME") != "" {
+		timeToRun, err2 = time.Parse("15:04:05", os.Getenv("TIME"))
+	}
+
+	if target == "" || method == "" || err1 != nil || err2 != nil || interval < 0 {
+		return -1, map[string]string{}, timeToRun, errors.New("Environment variables were not set, returning error")
 	}
 
 	return interval, map[string]string{
 			"target": target,
 			"method": method},
+		timeToRun,
 		nil
 }
 
@@ -59,7 +91,7 @@ func DoCall(client *http.Client, settings map[string]string) error {
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
+	//defer resp.Body.Close()
 
 	if err != nil {
 		return fmt.Errorf("Error! Received error while contacting target! %s", err)
